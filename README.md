480da90c8807c5d4f8a342de093fafe7a024517e
Brendon Felix <brendon.felix@hp.com>
2024-12-06 11:32:37 -0700

    updated README and help strings

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# Serial monitor
+# spewcap
+
+Basic serial monitor program to capture serial spew (BIOS spew, Super I/O spew, etc). I kinda hate using TeraTerm, so this is intended to replace that.
+
+## Features
+
+- Use the TOML file or CLI arguments to configure the application (CLI arguments take precedent)
+- Easy-to-use key commands (`C` to clear, `S` to save, `Q` to quit, etc.)
+
+## Todo
+
+- [ ] Add serial input
+
+## Prerequisites
+
+- Rust and Cargo installed: [Install Rust](https://www.rust-lang.org/tools/install)
+- A USB serial port device (usually UART)
+
+## Installation
+
+1. Clone this repository:
+    ```sh
+    git clone https://github.com/yourusername/spewcap.git # UPDATE LINK!
+    cd spewcap
+    ```
+
+2. Build the project:
+    ```sh
+    cargo build --release
+    ```
+
+3. Place the executable and TOML file in another location:
+    ```sh
+    cp .\config.toml C:\Program Files\serust
+    ```
+
+## Usage
+
+1. Change settings in `cargo.toml` (optional)
+
+2. Start the program from a terminal/shell
+
+    With `config.toml` configured
+
+    ```sh
+    ./spewcap.exe
+    ```
+
+    With command line option flags (override `config.toml` if it exists)
+
+    ```sh
+    ./spewcap.exe -p <port-name> -b <baud-rate>
+    ```
+
+    To view all options use `-h` or `--help`
+
+    ```sh
+    ./spewcap.exe -h
+    ```
+    > Tip: In Windows Terminal settings, under 'Defaults > Advanced', increase 'History size' to 100,000 and save
+
+    > Use `CTRL+SHIFT+F` to search spew without saving a log
+
+3. Press `L` to list avaiable ports and `H` to view all key commands
+
+4. Quit the program using `Q`
+
+## Example
+
+```sh
+./spewcap.exe -p COM5 -b 115200
+```
--- src/settings.rs
+++ src/settings.rs
@@ -7,19 +7,19 @@ use serde::Deserialize;
 // #[command(author, version, about, long_about = None)]
 #[derive(Parser, Debug)]
 struct Args {
-    /// port name (eg. COM23 OR /dev/ttyUSB0)
+    /// Port name (eg. COM23 OR /dev/ttyUSB0)
     #[arg(short, long)]
     port: Option<String>,
 
-    /// baud rate
+    /// Baud rate
     #[arg(short, long)]
     baud_rate: Option<u32>,
 
-    // timestamps
+    /// Prepend timestamps in log
     #[arg(short, long)]
     timestamps: Option<bool>,
 
-    /// log folder path
+    /// Folder path to save logs
     #[arg(short, long)]
     log_folder: Option<String>,
 }
@@ -47,14 +47,24 @@ fn get_config(filename: String) -> Option<Config> {
 
 pub fn get_settings() -> Settings {
     let args = Args::parse();
-    let config = get_config("config.toml".to_string()).unwrap();
-    Settings {
-        port: args.port.or(config.port).unwrap_or("COM5".to_string()),
-        baud_rate: args.baud_rate.or(config.baud_rate).unwrap_or(115200),
-        timestamps: args.timestamps.or(config.timestamps).unwrap_or(false),
-        log_folder: args.log_folder.or(config.log_folder)
-            .unwrap_or(r"C:\Users\felixb\OneDrive - HP Inc\Debugs\Springs".to_string()),
+    if let Some(config) = get_config("config.toml".to_string()) {
+        Settings {
+            port: args.port.or(config.port).unwrap_or("COM5".to_string()),
+            baud_rate: args.baud_rate.or(config.baud_rate).unwrap_or(115200),
+            timestamps: args.timestamps.or(config.timestamps).unwrap_or(false),
+            log_folder: args.log_folder.or(config.log_folder)
+                .unwrap_or(r"C:\Users\felixb\OneDrive - HP Inc\Debugs\Springs".to_string()),
+        }
+    } else {
+        Settings {
+            port: args.port.unwrap_or("COM5".to_string()),
+            baud_rate: args.baud_rate.unwrap_or(115200),
+            timestamps: args.timestamps.unwrap_or(false),
+            log_folder: args.log_folder
+                .unwrap_or(r"C:\Users\felixb\OneDrive - HP Inc\Debugs\Springs".to_string()),
+        }
     }
+    
 }
 
 // pub fn set_config(filename: String) -> Result<()> {
